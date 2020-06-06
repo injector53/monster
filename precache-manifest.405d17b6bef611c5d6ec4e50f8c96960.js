64e69d2b37fda5225734060cac1d30284a0ece86
bawlasingh <63555099+bawlasingh@users.noreply.github.com>
2020-06-07 02:07:06 +0530

    Updates

---
+++ precache-manifest.405d17b6bef611c5d6ec4e50f8c96960.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06b5ac2efe1772cd71b24667929d595a",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "97961e559aed559f619c",
+    "url": "/monster/static/css/main.12edfad5.chunk.css"
+  },
+  {
+    "revision": "80de6e261c3a1f36230c",
+    "url": "/monster/static/js/2.05463ef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster/static/js/2.05463ef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97961e559aed559f619c",
+    "url": "/monster/static/js/main.2ff12352.chunk.js"
+  },
+  {
+    "revision": "2372332ace1a33aea30f",
+    "url": "/monster/static/js/runtime-main.7a78c98a.js"
+  }
+]);
